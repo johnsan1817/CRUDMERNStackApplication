e1ea197ba1fa080eaca8b90aa01edea72c74da6f
johnsan1817 <johnsan1817@gmail.com>
2023-07-31 18:01:12 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(10000, () => {
-    console.log('Express server started at port : 10000');
+app.listen(2050, () => {
+    console.log('Express server started at port : 2050');
 });
 
 app.use('/employee', employeeController);
