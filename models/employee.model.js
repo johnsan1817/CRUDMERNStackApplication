44855ea69c2f73e3df9bb63b8226af78d53b8b5e
johnsan1817 <johnsan1817@gmail.com>
2023-07-31 17:28:08 +0530

    employee.model.js file updated

--- models/employee.model.js
+++ models/employee.model.js
@@ -16,10 +16,12 @@ var employeeSchema = new mongoose.Schema({
     }
 });
 
-// Custom validation for email
+
 employeeSchema.path('email').validate((val) => {
     emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(val);
 }, 'Invalid e-mail.');
+
+
 
 mongoose.model('Employee', employeeSchema);
